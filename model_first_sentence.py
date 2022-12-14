c8c7f77b8c797d0e7f79a9cacf5f2422cb8382b1
soid <soid.exe@gmail.com>
2022-12-14 18:01:37 -0500

    30 annotated examples + naive first sentence model

---
+++ model_first_sentence.py
@@ -0,0 +1,38 @@
+# baseline model: just combine first sentence from all reviews
+import click
+import json
+import rouge
+
+
+def predict(reviews):
+    sum = ""
+    for r in reviews:
+        s = r.split(".", 1)[0]
+        sum += s + ". "
+    return sum
+
+
+@click.command()
+@click.argument("file_path", type=click.Path(exists=True))
+def main(file_path):
+    # read data
+    f = open(file_path, "r")
+    data = json.loads(f.read())
+    f.close()
+
+    # generate
+    hyp, ref = [], []
+    for entity in data:
+        hyp.append(predict(entity['reviews']))
+        ref.append(entity['summary'][0])
+
+    # evaluate
+    evaluator = rouge.Rouge(metrics=["rouge-n", "rouge-l"], max_n=2, limit_length=False, apply_avg=True,
+                            stemming=True, ensure_compatibility=True)
+    sums = evaluator.get_scores(hyp, ref).items()
+    for metric, val in sums:
+        print("  ", metric, ":", "%.2f" % (val['f']*100))
+
+
+if __name__ == '__main__':
+    main()
